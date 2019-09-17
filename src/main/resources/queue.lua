788c3216bdf980539d1cb466d4f69a7883d98e58
Olli Kuonanoja <olli.kuonanoja@nosto.com>
2019-09-17 20:25:15 +0300

    Re-use the payload hash between visible and invisible messages
    Avoids unnecessary memory copies now that the structures are similar.

--- src/main/resources/queue.lua
+++ src/main/resources/queue.lua
@@ -3,20 +3,19 @@ local private = {}
 local keyseparator = "KEY_PAYLOAD_SEPARATOR"
 
 function public.enqueue(slot, queue, time, nexttime, tenant, key, payload)
-    local deduplicator_key = private.deduplicator_key(slot, queue, tenant)
+    local visible_key = private.visible_key(slot, queue, tenant)
     redis.call("hset", private.period(slot, queue), tenant, nexttime - time)
+    redis.call("hset", private.payload_key(slot, queue, tenant), key, payload)
 
-    if redis.call("zrank", deduplicator_key, key) == false then
+    if redis.call("zrank", visible_key, key) == false then
         local schedule_key = private.schedule_key(slot, queue)
 
         if not redis.call("zrank", schedule_key, tenant) then
             redis.call("zadd", schedule_key, nexttime, tenant)
         end
-        redis.call("hset", private.visible_key(slot, queue, tenant), key, payload)
-        redis.call("zadd", deduplicator_key, time, key)
+        redis.call("zadd", visible_key, time, key)
         return true
     else
-        redis.call("hset", private.visible_key(slot, queue, tenant), key, payload)
         return false
     end
 end
@@ -31,16 +30,14 @@ function public.dequeue(slot, queue, time, maxkeys)
 
         local invisible = redis.call("zrangebyscore", invisible_key, "-inf", time, "LIMIT", 0, 1)
         if next(invisible) == nil then
-            local visible = redis.call("zrangebyscore", private.deduplicator_key(slot, queue, tenant), "-inf", "+inf", "LIMIT", 0, 1)
+            local visible = redis.call("zrangebyscore", private.visible_key(slot, queue, tenant), "-inf", "+inf", "LIMIT", 0, 1)
 
             if next(visible) then
                 local _, key = next(visible)
-                local payload = redis.call("hget", private.visible_key(slot, queue, tenant), key)
-                redis.call("hdel", private.visible_key(slot, queue, tenant), key)
-                redis.call("zrem", private.deduplicator_key(slot, queue, tenant), key)
+                local payload = redis.call("hget", private.payload_key(slot, queue, tenant), key)
+                redis.call("zrem", private.visible_key(slot, queue, tenant), key)
 
                 redis.call("zadd", invisible_key, nexttime, key)
-                redis.call("hset", private.invisible_payload_key(slot, queue, tenant), key, payload)
 
                 result[#result + 1] = tenant
                 result[#result + 1] = key
@@ -49,7 +46,7 @@ function public.dequeue(slot, queue, time, maxkeys)
             end
         else
             local _, key = next(invisible)
-            local payload = redis.call("hget", private.invisible_payload_key(slot, queue, tenant), key)
+            local payload = redis.call("hget", private.payload_key(slot, queue, tenant), key)
             redis.call("zadd", schedule_key, nexttime, tenant)
             result[#result + 1] = tenant
             result[#result + 1] = key
@@ -61,10 +58,11 @@ end
 
 function public.ack(slot, queue, tenant, key)
     local invisible_key = private.invisible_key(slot, queue, tenant)
-    redis.call("zrem", private.deduplicator_key(slot, queue, tenant), key)
-    redis.call("hdel", private.invisible_payload_key(slot, queue, tenant), key)
+    local payload_key = private.payload_key(slot, queue, tenant)
+    redis.call("zrem", private.visible_key(slot, queue, tenant), key)
+    redis.call("hdel", payload_key, key)
     redis.call("zrem", invisible_key, key)
-    if redis.call("hlen", private.visible_key(slot, queue, tenant)) == 0 and redis.call("zcard", invisible_key) == 0 then
+    if redis.call("hlen", payload_key) == 0 then
         redis.call("zrem", private.schedule_key(slot, queue), tenant)
     end
 end
@@ -74,8 +72,8 @@ function public.queuestats(slot, queue)
     local result = {}
     for _, tenant in ipairs(tenants) do
         result[#result + 1] = tenant
-        result[#result + 1] = redis.call("hlen", private.invisible_payload_key(slot, queue, tenant))
-        result[#result + 1] = redis.call("hlen", private.visible_key(slot, queue, tenant))
+        result[#result + 1] = redis.call("zcard", private.invisible_key(slot, queue, tenant))
+        result[#result + 1] = redis.call("zcard", private.visible_key(slot, queue, tenant))
     end
     return result
 end
@@ -84,20 +82,16 @@ function private.schedule_key(slot, queue)
     return "mq:{" .. slot .. "}:" .. queue .. ":schedule"
 end
 
-function private.deduplicator_key(slot, queue, tenant)
-    return "mq:{" .. slot .. "}:" .. queue .. ":deduplicator:" .. tenant
-end
-
 function private.visible_key(slot, queue, tenant)
-    return "mq:{" .. slot .. "}:visible:" .. queue .. ":" .. tenant
+    return "mq:{" .. slot .. "}:" .. queue .. ":visible:" .. tenant
 end
 
 function private.invisible_key(slot, queue, tenant)
     return "mq:{" .. slot .. "}:invisible:" .. queue .. ":" .. tenant
 end
 
-function private.invisible_payload_key(slot, queue, tenant)
-    return "mq:{" .. slot .. "}:invisible:payload:" .. queue .. ":" .. tenant
+function private.payload_key(slot, queue, tenant)
+    return "mq:{" .. slot .. "}:payload:" .. queue .. ":" .. tenant
 end
 
 function private.period(slot, queue)
