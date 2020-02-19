dd22f5eb13443061c3ee4072254caf2351ef48ff
Gary Madden <gary.madden@nosto.com>
2020-02-19 14:31:06 +0200

    Checkstyle

--- src/test/java/com/nosto/redis/queue/LowLevelScriptTest.java
+++ src/test/java/com/nosto/redis/queue/LowLevelScriptTest.java
@@ -19,7 +19,6 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
