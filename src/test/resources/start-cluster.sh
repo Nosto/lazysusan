c85dc4db4c0f7bd70b846d6a9da55e1fc07c0dd2
Gary Madden <gary.madden@nosto.com>
2020-03-27 16:33:16 +0200

    Code cleanup

--- src/test/resources/start-cluster.sh
+++ src/test/resources/start-cluster.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-for PORT in 7000 7001 7002 7003 7004 7005; do
+for PORT in 7001 7002 7003 7004 7005 7006; do
   redis-server --port $PORT --cluster-enabled yes --cluster-config-file nodes-$PORT.conf \
    --cluster-node-timeout 1000 --appendonly yes --appendfilename appendonly-$PORT.aof \
    --dbfilename dump-$PORT.rdb --logfile $PORT.log --daemonize yes
 done
 
-echo "yes" | eval redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 \
-127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 \
---cluster-replicas 1
+echo "yes" | eval redis-cli --cluster create 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 \
+127.0.0.1:7005 127.0.0.1:7006 --cluster-replicas 1
 
-tail -f 7000.log
+tail -f 7001.log
