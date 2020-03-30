3391faae9645d3075223d091de30dcc96f01c2f0
Gary Madden <gary.madden@nosto.com>
2020-03-30 09:59:25 +0300

    Revert "Revert "Making start-cluster.sh dynamic""
    This reverts commit f88c22a50e343e33f602f56e8e2bbf8f12bf00f0.

--- src/test/resources/start-cluster.sh
+++ src/test/resources/start-cluster.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-for PORT in 7001 7002 7003 7004 7005 7006; do
+if "$1" == "yes"; then
+  CREATE_CLUSTER=true
+fi
+
+shift
+
+NODES=""
+for PORT in "$@"; do
   redis-server --port $PORT --cluster-enabled yes --cluster-config-file nodes-$PORT.conf \
    --cluster-node-timeout 1000 --appendonly yes --appendfilename appendonly-$PORT.aof \
    --dbfilename dump-$PORT.rdb --logfile $PORT.log --daemonize yes
+   NODES="$NODES 127.0.0.1:$PORT"
 done
 
-echo "yes" | eval redis-cli --cluster create 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 \
-127.0.0.1:7005 127.0.0.1:7006 --cluster-replicas 1
+if $CREATE_CLUSTER; then
+  echo "yes" | eval redis-cli --cluster create $NODES --cluster-replicas 1
+fi
 
-tail -f 7001.log
+tail -f $1.log
